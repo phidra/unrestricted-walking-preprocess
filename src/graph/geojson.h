7c60676b3a7d0e2ce3ae00b2b7ebddf6747a5257
Philippe DRABCZUK <philippe@drabczuk.fr>
2021-04-27 14:40:54 +0200

    dependency to rapidjson is now private

--- src/graph/geojson.h
+++ src/graph/geojson.h
@@ -11,6 +11,7 @@
 #include <boost/geometry/geometries/geometries.hpp>
 
 #include "graph/types.h"
+#include "graph/polygon.h"
 #include "graph/graphtypes.h"
 
 namespace my {
@@ -18,51 +19,6 @@ namespace my {
 void dump_geojson_graph(std::ostream& out, std::vector<Edge> const& edges, bool allow_unranked);
 void dump_geojson_stops(std::ostream& out, std::vector<StopWithClosestNode> const& stops);
 std::vector<Edge> parse_geojson_graph(std::istream& in);
-
-template <typename PointsOfLine>
-void dump_geojson_line(std::ostream& out, PointsOfLine const& pointsOfLine) {
-    rapidjson::Document doc(rapidjson::kObjectType);
-    rapidjson::Document::AllocatorType& a = doc.GetAllocator();
-    doc.AddMember("type", "FeatureCollection", a);
-
-    rapidjson::Value features(rapidjson::kArrayType);
-
-    // coordinates :
-    rapidjson::Value coordinates(rapidjson::kArrayType);
-
-    for (auto point = boost::begin(pointsOfLine); point != boost::end(pointsOfLine); ++point) {
-        double lng = boost::geometry::get<0>(*point);
-        double lat = boost::geometry::get<1>(*point);
-        rapidjson::Value coords(rapidjson::kArrayType);
-        coords.PushBack(rapidjson::Value(lng), a);
-        coords.PushBack(rapidjson::Value(lat), a);
-        coordinates.PushBack(coords, a);
-    }
-
-    // geometry :
-    rapidjson::Value geometry(rapidjson::kObjectType);
-    geometry.AddMember("coordinates", coordinates, a);
-    geometry.AddMember("type", "LineString", a);
-
-    // properties :
-    rapidjson::Value properties(rapidjson::kObjectType);
-    properties.AddMember("nb_points", rapidjson::Value(boost::geometry::num_points(pointsOfLine)), a);
-
-    // feature :
-    rapidjson::Value feature(rapidjson::kObjectType);
-    feature.AddMember("type", "Feature", a);
-    feature.AddMember("geometry", geometry, a);
-    feature.AddMember("properties", properties, a);
-    features.PushBack(feature, a);
-
-    doc.AddMember("features", features, a);
-
-    // dumping :
-    rapidjson::OStreamWrapper out_wrapper(out);
-    rapidjson::PrettyWriter<rapidjson::OStreamWrapper> writer(out_wrapper);
-    doc.Accept(writer);
-
-    out << std::endl;
-}
+void dump_geojson_line(std::ostream& out, BgPolygon::ring_type const&);
 
 }  // namespace my
